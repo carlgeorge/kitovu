c0b74b9b4556bde59212680ef6e46194a600c2a6
carlgeorge <carl.george@rackspace.com>
2015-06-06 17:48:34 -0500

    add minimal and bulk formatting options

--- kitovu/cli.py
+++ kitovu/cli.py
@@ -1,3 +1,4 @@
+import json
 import click
 from .engine import Engine
 
@@ -14,8 +15,10 @@ def cli(context, profile):
 @click.pass_obj
 @click.option('--page', type=int)
 @click.option('--per-page', type=int)
+@click.option('--minimal', '-m', is_flag=True)
+@click.option('--bulk', '-b', is_flag=True)
 @click.argument('uri')
-def get(api, page, per_page, uri):
+def get(api, page, per_page, minimal, bulk, uri):
     ''' perform an HTTP GET on the given URI '''
     if page or per_page:
         params = []
@@ -24,7 +27,17 @@ def get(api, page, per_page, uri):
         if per_page:
             params.append('per_page={}'.format(per_page))
         uri = '{}?{}'.format(uri, '&'.join(params))
-    for page in api.get(uri):
-        data = page.json()
-        names = [item.get('name') for item in data]
-        click.echo(names)
+    if bulk:
+        data = []
+        for page in api.get(uri):
+            if minimal:
+                data.extend([item.get('name') for item in page.json()])
+            else:
+                data.extend(page.json())
+        click.echo(json.dumps(data, sort_keys=True))
+    else:
+        for page in api.get(uri):
+            if minimal:
+                click.echo([item.get('name') for item in page.json()])
+            else:
+                click.echo(page.json())
